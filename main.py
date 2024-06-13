74447c37e487b72de3ace1fcc06ff64f2086362d
mishchenkoillya <mishchenkoillya1505@gmail.com>
2024-06-13 14:57:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ window = display.set_mode((W, H))
 bg = transform.scale(image.load('fon.jpg'), (W, H))# трансформуємо картинку в розміри вікна
 
 clock = time.Clock()# лічильник кадрів
+fontr=init()
+font1 = font.SysFont('Aria', 30, bold=True)
+font2 = font.SysFont('Aria', 40, italic=True)
 
 class GameSprite(sprite.Sprite):# базовий клас для всіх спрайтів
     def __init__(self, img, x, y, width, height, speed_x, speed_y):# конструктор класу
@@ -52,6 +55,11 @@ player1 = Player('rekat.png', 5, 5, 30, 100, 10, 10)
 player2 = Player('rekat.png',750 , 250, 30, 100, 10, 10)
 ball = Ball('ball.png', W / 2, H / 2, 70, 70, 3, 3)
 
+player1_points = 0
+player2_points = 0
+
+
+
 game = True
 while game:
     window.blit(bg, (0, 0))
@@ -63,6 +71,12 @@ while game:
 
     ball.draw()
     ball.move()
+    
+    player1_txt = font2.render(str(player1_points), 1, (255,255,255))
+    player2_txt = font2.render(str(player2_points), 1, (255,255,255))
+    window.blit(player1_txt,(30, 10))
+    window.blit(player2_txt,(770, 10))
+
     for e in event.get():
         if e.type == QUIT:
             game = False
@@ -71,6 +85,17 @@ while game:
         ball.speed_x *= -1
     if ball.rect.y < 0 or ball.rect.y > H - ball.height:
         ball.speed_y *= -1
+
+
+    if ball.rect.x < 0:
+        player2_points += 1
+        ball.rect.x = W/2
+        ball.rect.y = H/2
+
+    if ball.rect.x > W:
+        player1_points += 1
+        ball.rect.x = W/2
+        ball.rect.y = H/2
         
 
     clock.tick(100)
